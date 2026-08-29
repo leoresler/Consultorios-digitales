79338a2510b06512c10474cd6c4d21a37f161322
Pocke197 <kevin.lee.sebastian@gmail.com>
2026-08-29 17:37:59 -0300

    feat: Prisma Set Up

---
+++ apps/api/prisma.config.ts
@@ -0,0 +1,10 @@
+import { config } from 'dotenv';
+config();
+
+import { defineConfig } from 'prisma/config';
+
+export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+});
